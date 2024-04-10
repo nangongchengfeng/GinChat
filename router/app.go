8d3c5bc43a09f7dfd14eae9a765323c9b220aced
南宫乘风 <1794748404@qq.com>
2024-04-10 10:05:38 +0800

    [增加] 前端页面添加

--- router/app.go
+++ router/app.go
@@ -10,14 +10,20 @@ import (
 
 func Router() *gin.Engine {
 	r := gin.Default()
+	// 设置网页存放目录
+	r.LoadHTMLGlob("views/**/*")
 	docs.SwaggerInfo.BasePath = ""
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	r.GET("/index", service.GetIndex)
+
 	r.GET("/user/getUserList", service.GetUserList)
 	r.GET("/user/createUser", service.CreateUser)
 	r.GET("/user/deleteUser", service.DeleteUser)
 	r.POST("/user/updateUser", service.UpdateUser)
 	r.POST("/user/findUserByNameAndPwd", service.FindUserByNameAndPwd)
 	r.GET("/user/sendMsg", service.SendMsg)
+	// 首页
+	r.GET("/", service.GetIndex)
+	r.GET("/index", service.GetIndex)
+	r.GET("/toRegister", service.ToRegister)
 	return r
 }
--- service/index.go
+++ service/index.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/gin-gonic/gin"
+	"html/template"
 )
 
 // GetIndex
@@ -9,7 +10,25 @@ import (
 // @Success 200 {string} welcome
 // @Router /index [get]
 func GetIndex(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Hello World",
-	})
+	ind, err := template.ParseFiles(
+		"./views/chat/index.html", // 调整为正确的路径
+		"./views/chat/head.html",  // 调整为正确的路径
+	)
+	if err != nil {
+		panic(err)
+	}
+	ind.Execute(c.Writer, "index")
+	//c.HTML(http.StatusOK, "index.html", gin.H{"title": "我是测试", "ce": "123456"})
+
+}
+
+func ToRegister(c *gin.Context) {
+	ind, err := template.ParseFiles("views/user/register.html")
+	if err != nil {
+		panic(err)
+	}
+	ind.Execute(c.Writer, "register")
+	// c.JSON(200, gin.H{
+	// "message": "welcome !! ",
+	// })
 }
